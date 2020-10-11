936b55e13359a70c985fd9346c32c5a39fe9bc34
mamudsamiul <samiulmamud1995@gmail.com>
2020-10-11 19:24:30 +0530

    (Samiul Mamud) Added: Simple Test case is created to make a simple linkedlist

--- src/main/java/com/capgemini/linkedlist/Executer.java
+++ src/main/java/com/capgemini/linkedlist/Executer.java
@@ -3,7 +3,7 @@ package com.capgemini.linkedlist;
 public class Executer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
 
--- src/test/java/com/capgemini/linkedlisttest/TestCase.java
+++ src/test/java/com/capgemini/linkedlisttest/TestCase.java
@@ -4,11 +4,22 @@
 package com.capgemini.linkedlisttest;
 
 import org.junit.Test;
+
+import com.capgemini.linkedlist.Node;
+
+import junit.framework.Assert;
+
 import static org.junit.Assert.*;
 
 public class TestCase {
 	@Test
-	public void testSomeLibraryMethod() {
-
+	public void addingThreeNumberShouldPass() {
+		Node<Integer> firstNode = new Node<>(56);
+		Node<Integer> secondNode = new Node<>(30);
+		Node<Integer> thirdNode = new Node<>(70);
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
+		Assert.assertTrue(result);
 	}
 }
